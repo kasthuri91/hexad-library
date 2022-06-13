3730692e3675da80b9a39ecf9bd44ebee1151f68
kasthuri@91 <chathum99@gmail.com>
2022-06-13 12:21:53 +0530

    Initial Commit

---
+++ src/main/java/com/hexad/library/HexadLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.hexad.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexadLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexadLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexad/library/HexadLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexad.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexadLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
